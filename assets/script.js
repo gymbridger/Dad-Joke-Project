e9a218d355c3d9780d3a1c75dfbf2aa0ac681ddd
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-05-30 21:32:39 -0600

    JS functionality to randomize or select specific API- Issue#25

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,58 @@
 
 const jokeButton = document.querySelector(".btn-primary");
 const jokeHolder = document.querySelector("#populate-joke");
-const dadJokes = "http://icanhazdadjoke.com";
-const chuckJokes = "https://api.chucknorris.io/jokes/random";
+const dadJokesURL = "http://icanhazdadjoke.com";
+const chuckJokesURL = "https://api.chucknorris.io/jokes/random";
+const randomButton = document.querySelector("#random");
+const dadButton = document.querySelector("#dad");
+const chuckButton = document.querySelector("#chuck");
+
+//start array set to 'random' configuration by default, use a choice function to determine final array to fetch jokes from
+apiArray = [dadJokesURL, chuckJokesURL];
 
 
-selectorArray= [];
+//button clicks reset array then push chosen URLs. NOTE** This method does not scale easily if more APIs are added in the future and the user wants to filter multiple APIs. **
+randomButton.addEventListener("click", function() {
+    apiArray = []
+    apiArray.push(dadJokesURL, chuckJokesURL);
+    console.log(apiArray);
+})
+
+dadButton.addEventListener("click", function() {
+    apiArray = []
+    apiArray.push(dadJokesURL);
+    console.log(apiArray);
+})
+
+chuckButton.addEventListener("click", function() {
+    apiArray = []
+    apiArray.push(chuckJokesURL);
+    console.log(apiArray);
+})
+
+
+function getAPI() {
+    const randomizer = Math.floor(Math.random() * apiArray.length);
+    const newAPI = apiArray[randomizer];
+    console.log(newAPI)
+    return newAPI;
+    
+};
+
 
 async function fetchJoke() {
-    const response = await fetch("http://icanhazdadjoke.com", {
+
+    //uses whichever array is created from the buttons to provide the URLs
+    var newAPI = getAPI();
+
+    const response = await fetch(newAPI, {
         headers: {
             Accept: "application/json",
         },
         });
     const joke = await response.json();
     console.log(joke)
-    jokeHolder.textContent = joke.joke;    
+    jokeHolder.textContent = joke.joke || joke.value;  
     
 };
 
@@ -24,30 +61,3 @@ async function handleClick() {
 };
 
 jokeButton.addEventListener("click", handleClick);
-
-
-
-
-
-
-
-    
-
-// function getChuck() {
-
-//     var requestURL = chuckJokes;
-
-//     fetch(requestURL)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (chuckJoke) {
-//             console.log(chuckJoke.value);
-//             jokeHolder.textContent = chuckJoke.value
-//         })
-// };
-
-
-
-// jokeButton.addEventListener("click", getChuck);
-
