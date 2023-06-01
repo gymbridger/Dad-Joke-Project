38e380718f415fc0103aff5be5218e49af2fabdd
Reiley Burnitt <burnittreiley@gmail.com>
2023-05-31 21:10:23 -0600

    moved css to assets folder

--- assets/script.js
+++ assets/script.js
@@ -65,19 +65,10 @@ async function fetchJoke() {
     jokeHolder.textContent = joke.joke || joke.value;  
     
 };
-async function fetchJoke2() {
-    const response = await fetch("https://api.chucknorris.io/jokes/random", {
-        headers: {
-            Accept: "application/json",
-        },
-        });
-    const joke = await response.json();
-    jokeHolder.textContent = joke.joke; 
-    };
 
-if (dadButton = )
 async function handleClick() {
     await fetchJoke();
 };
+
 
 jokeButton.addEventListener("click", handleClick);
