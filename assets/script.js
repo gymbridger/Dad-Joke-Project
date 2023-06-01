4a732e2d63e6eca9c2d55b9869a2805b07064b0d
Bridger Daw <bridgerdaw@yahoo.com>
2023-06-01 09:22:25 -0600

    fixed buttons to match html

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 const jokeButton = document.querySelector(".btn-primary");
 const jokeHolder = document.querySelector("#populate-joke");
 const dadJokesURL = "http://icanhazdadjoke.com";
@@ -10,73 +9,66 @@ const chuckButton = document.querySelector("#chuck");
 //start array set to 'random' configuration by default, use a choice function to determine final array to fetch jokes from
 apiArray = [dadJokesURL, chuckJokesURL];
 
-
-
 //button clicks reset array then push chosen URLs. NOTE** This method does not scale easily if more APIs are added in the future and the user wants to filter multiple APIs. **
-randomButton.addEventListener("click", function() {
-    apiArray = []
-    this.setAttribute("class", "btn btn-success me-2");
-    dadButton.setAttribute("class", "btn btn-outline-success me-2");
-    chuckButton.setAttribute("class", "btn btn-outline-success me-2");
-    apiArray.push(dadJokesURL, chuckJokesURL);
-    console.log(apiArray);
-})
+randomButton.addEventListener("click", function () {
+  apiArray = [];
+  this.setAttribute("class", "btn btn-dark me-2");
+  dadButton.setAttribute("class", "btn btn-light btn-outline-dark me-2");
+  chuckButton.setAttribute("class", "btn btn-light btn-outline-dark me-2");
+  apiArray.push(dadJokesURL, chuckJokesURL);
+  console.log(apiArray);
+});
 
-dadButton.addEventListener("click", function() {
-    apiArray = []
-    this.setAttribute("class", "btn btn-success me-2");
-    randomButton.setAttribute("class", "btn btn-outline-success me-2");
-    chuckButton.setAttribute("class", "btn btn-outline-success me-2");
-    apiArray.push(dadJokesURL);
-    console.log(apiArray);
-})
+dadButton.addEventListener("click", function () {
+  apiArray = [];
+  this.setAttribute("class", "btn btn-dark me-2");
+  randomButton.setAttribute("class", "btn btn-light btn-outline-dark me-2");
+  chuckButton.setAttribute("class", "btn btn-light btn-outline-dark me-2");
+  apiArray.push(dadJokesURL);
+  console.log(apiArray);
+});
 
-chuckButton.addEventListener("click", function() {
-    apiArray = []
-    this.setAttribute("class", "btn btn-success me-2");
-    randomButton.setAttribute("class", "btn btn-outline-success me-2");
-    dadButton.setAttribute("class", "btn btn-outline-success me-2");
-    apiArray.push(chuckJokesURL);
-    console.log(apiArray);
-})
-
+chuckButton.addEventListener("click", function () {
+  apiArray = [];
+  this.setAttribute("class", "btn btn-dark me-2");
+  randomButton.setAttribute("class", "btn btn-light btn-outline-dark me-2");
+  dadButton.setAttribute("class", "btn btn-light btn-outline-dark me-2");
+  apiArray.push(chuckJokesURL);
+  console.log(apiArray);
+});
 
 function getAPI() {
-    const randomizer = Math.floor(Math.random() * apiArray.length);
-    const newAPI = apiArray[randomizer];
-    console.log(newAPI)
-    return newAPI;
-    
-};
-
+  const randomizer = Math.floor(Math.random() * apiArray.length);
+  const newAPI = apiArray[randomizer];
+  console.log(newAPI);
+  return newAPI;
+}
 
 async function fetchJoke() {
+  //uses whichever array is created from the buttons to provide the URLs
+  var newAPI = getAPI();
 
-    //uses whichever array is created from the buttons to provide the URLs
-    var newAPI = getAPI();
-
-    const response = await fetch(newAPI, {
-        headers: {
-            Accept: "application/json",
-        },
-        });
-    const joke = await response.json();
-    console.log(joke)
-    jokeHolder.textContent = joke.joke || joke.value;  
-    
-};
+  const response = await fetch(newAPI, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const joke = await response.json();
+  console.log(joke);
+  jokeHolder.textContent = joke.joke || joke.value;
+}
 async function fetchJoke2() {
-    const response = await fetch("https://api.chucknorris.io/jokes/random", {
-        headers: {
-            Accept: "application/json",
-        },
-        });
-    const joke = await response.json();
-    jokeHolder.textContent = joke.joke; 
-    };
+  const response = await fetch("https://api.chucknorris.io/jokes/random", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const joke = await response.json();
+  jokeHolder.textContent = joke.joke;
+}
 
 async function handleClick() {
-        await fetchJoke();
+  await fetchJoke();
 }
 
 jokeButton.addEventListener("click", handleClick);
